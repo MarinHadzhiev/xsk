6ba1aa516cc2ba24373038bd625658f3ed44010f
Fluctuationqt <outrageousxqt@gmail.com>
2022-03-10 15:26:24 +0200

    fix: call handleHDBTableFunctions only once per project, not per file

--- modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/server/migration/api/migration-service.mjs
+++ modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/server/migration/api/migration-service.mjs
@@ -593,7 +593,7 @@ export class MigrationService {
             projectNames.add(projectName);
         }
 
-        for (const projectName of projectNames) {
+        for(const projectName of projectNames) {
             this.handleHDBTableFunctions(workspace, projectName);
         }
     }
